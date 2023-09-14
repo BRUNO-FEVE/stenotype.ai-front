ac9456152bc4eda2cce825935b1d6e84a950f7cd
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-09-14 12:36:05 -0300

    converted mp4 to mp3, and maked request to api: /videos and / transcription

--- src/components/video-consversor-form.tsx
+++ src/components/video-consversor-form.tsx
@@ -3,10 +3,21 @@ import { Button } from "./ui/button"
 import { Label } from "./ui/label"
 import { Separator } from "./ui/separator"
 import { Textarea } from "./ui/textarea"
-import { ChangeEvent, useMemo, useState } from "react"
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react"
+import { getFfmpeg } from "@/lib/ffmpeg"
+import { fetchFile } from '@ffmpeg/util'
+import { api } from "@/lib/axios"
+
+// interface ButtonStatesProps {
+//   wating: 'wating',
+//   transcripting: 'transcripting',
+//   finished: 'finished'
+// }
 
 export function VideoConversorForm() {
   const [ video, setVideo ] = useState<File | null>(null)
+  const promptRef = useRef<HTMLTextAreaElement>(null)
+
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -27,12 +38,70 @@ export function VideoConversorForm() {
     return URL.createObjectURL(video)
   }, [video])
 
+  async function convertAudioToVideo (video: File) {
+    console.log('Convert Started.')
+
+    const ffmpeg = await getFfmpeg()
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+    ffmpeg.on('progress', progress => {
+      console.log('Convert progress: ' + Math.round(progress.progress * 100))
+    })
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3'
+    ])
+
+    const data = await ffmpeg.readFile('output.mp3')
+
+    const audioFileBlob = new Blob([data], { type: 'audio/mpeg' })
+    const audioFile = new File([audioFileBlob], 'audio.mp3', { type: 'audio/mpeg' })
+
+    console.log('Convert finished.')
+
+    return audioFile
+  }
+
+  async function handleSubimit (event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt= promptRef.current?.value
+
+    if (!video) {
+      return
+    }
+
+    const audioFile = await convertAudioToVideo(video)
+
+    const data = new FormData()
+
+    data.append('file', audioFile)
+
+    const response = await api.post('/videos', data)
+    const videoId = response.data.video.id
+
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt: prompt
+    })
+
+    console.log('finalizou')
+  }
+
   return (
-    <form className="space-y-3">
+    <form onSubmit={handleSubimit} className="space-y-3">
       <label htmlFor="video" className="aspect-video flex border border-dashed text-sm text-muted-foreground flex-col justify-center items-center hover:bg-primary/5 cursor-pointer">
-        {video ? 
+        {previewURL ? 
           <>
-            <video src={previewURL ? previewURL : ''} />
+            <video src={previewURL} controls={false} className="pointer-events-none"/>
           </>
         : 
           <>
@@ -45,9 +114,14 @@ export function VideoConversorForm() {
       <Separator />
       <div className=" space-y-1">
         <Label className="text-sm" htmlFor="transcription_prompt">Prompt de transcrição</Label>
-        <Textarea id="transcription_prompt" className="resize-none h-14" placeholder="Inclua palavras-chave mencionadas no video separadas por virgula (,)" />
+        <Textarea 
+          ref={promptRef}
+          id="transcription_prompt" 
+          className="resize-none h-14" 
+          placeholder="Inclua palavras-chave mencionadas no video separadas por virgula (,)" 
+        />
       </div>
-      <Button className="w-full gap-4">
+      <Button type="submit" className="w-full gap-4">
           Carregar vídeo
           <Upload className="h-4 w-4" />
       </Button>
