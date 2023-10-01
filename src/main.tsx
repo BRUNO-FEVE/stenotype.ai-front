58e9f9ffe971b17d756d5b767d4f00e08e04f277
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-09-30 22:33:38 -0300

    add router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { App } from './App.tsx'
 import './index.css'
-import Home from './pages/home.tsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import AppRouter from './router.tsx'
+import ThemeProvider from './context/theme-context.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <ThemeProvider>
+        <AppRouter />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>,
 )
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
+import Prompt from './pages/prompt'
 
 export default function AppRouter() {
   return (
     <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='prompt' element={<Prompt />} />
     </Routes>
   )
 }
