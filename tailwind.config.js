66fed762a940508dad7cf34da17e4d680633899b
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-10-08 00:19:32 -0300

    tailwind hide scroll bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   theme: {
     extend: {
       textColor: {
         skin: {
           base: 'var(--color-text-base)',
           muted: 'var(--color-text-muted)',
-          inverted: 'var(--color-background)'
-        }
+          inverted: 'var(--color-background)',
+        },
       },
       backgroundColor: {
         skin: {
@@ -26,20 +26,20 @@ module.exports = {
           'button-accent': 'var(--color-button-accent)',
           'button-accent-hover': 'var(--color-button-accent-hover)',
           'button-muted': 'var(--color-button-muted)',
-        }
+        },
       },
       gradientColorStops: {
         skin: {
-          hue: 'var(--color-fill)'
-        }
+          hue: 'var(--color-fill)',
+        },
       },
       borderColor: {
         skin: {
-          "bg-muted": "var(--color-text-muted)" 
-        }
-      }
+          'bg-muted': 'var(--color-text-muted)',
+        },
+      },
     },
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
 }
