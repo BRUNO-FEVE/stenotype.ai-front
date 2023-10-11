b9cfbaedb0eb4d7488cd6fd727dcfa4e077da6f4
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-10-11 12:31:00 -0300

    new button no nav to control station

--- src/pages/documentation.tsx
+++ src/pages/documentation.tsx
@@ -4,7 +4,10 @@ import { useState } from 'react'
 import { documentationContentList } from '@/data/documentation-data'
 import { ContentUnit } from '@/lib/documentation-list'
 import Separator from '@/components/ui/separator'
+import PageButton from '@/components/ui/button'
 
+const GET_STARTED = documentationContentList.head
+const GET_STARTED_LAST_CHILD = documentationContentList.lastChild()
 const GET_STARTED_STEPS = documentationContentList.listChilds()
 
 export default function Documentation() {
@@ -16,6 +19,17 @@ export default function Documentation() {
     setPageContent(newPageContent)
   }
 
+  function handleButtonMessage(): string {
+    switch (pageContent) {
+      case GET_STARTED:
+        return "Let's Start"
+      case GET_STARTED_LAST_CHILD:
+        return "Let's Try"
+      default:
+        return 'Next Step'
+    }
+  }
+
   const handlePageContent = (comand: string): void => {
     switch (comand) {
       case 'next':
@@ -23,12 +37,9 @@ export default function Documentation() {
           if (prev?.next) {
             return prev.next
           }
-          return documentationContentList.head
+          return null
         })
         break
-      case '':
-        break
-
       default:
         break
     }
@@ -66,19 +77,23 @@ export default function Documentation() {
           </Collapsible.Content>
         </Collapsible.Root>
       </div>
-      <div className="p-6 h-fit w-full md:w-4/5 lg:w-3/5 flex flex-col items-center gap-10">
+      <div className="p-6 h-fit w-full md:w-4/5 lg:w-3/5 flex flex-col items-end gap-10">
         {pageContent?.component}
-        <Separator orientation="horizontal" className="w-full" />
-        <button
-          onClick={() => {
-            handlePageContent('next')
-          }}
-          className="px-20 py-2 ml-auto text-white bg-skin-button-accent hover:bg-skin-button-accent-hover rounded-md"
-        >
-          {pageContent === documentationContentList.head
-            ? "Let's Start"
-            : 'Next Step'}
-        </button>
+        <Separator orientation="horizontal" />
+        {pageContent === GET_STARTED_LAST_CHILD ? (
+          <PageButton variant="default" to="/conversor">
+            Let's Try
+          </PageButton>
+        ) : (
+          <button
+            onClick={() => {
+              handlePageContent('next')
+            }}
+            className="px-20 py-2 ml-auto text-white bg-skin-button-accent hover:bg-skin-button-accent-hover rounded-md"
+          >
+            {handleButtonMessage()}
+          </button>
+        )}
       </div>
     </DefaultPageLayout>
   )
