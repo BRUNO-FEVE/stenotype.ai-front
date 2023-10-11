730b7c603e5a0fa258a9bc3f410bfce698a838fd
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-10-11 12:30:30 -0300

    button page new variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,19 +3,42 @@ import React, { ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 import { twMerge } from 'tailwind-merge'
 
+interface VariantProps {
+  style: string
+  content: ReactNode
+}
+
 interface NewPageButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: string
   to: string
 }
 
-export default function PageButton({ to, ...props }: NewPageButtonProps) {
+export default function PageButton({
+  variant,
+  to,
+  children,
+  ...props
+}: NewPageButtonProps) {
+  const variants: { [key: string]: VariantProps } = {
+    default: {
+      style:
+        'px-20 py-2 ml-auto text-white bg-skin-button-accent hover:bg-skin-button-accent-hover rounded-md',
+      content: children,
+    },
+    fixed: {
+      style:
+        'bg-skin-bg-base-foreground h-14 w-14 rounded-full fixed right-5 bottom-5 flex justify-center items-center',
+      content: <ArrowRightIcon className="text-skin-inverted w-8 h-8" />,
+    },
+  }
+
+  const style = variants[variant].style
+  const content = variants[variant].content
   return (
     <Link to={to}>
-      <button
-        {...props}
-        className="bg-skin-bg-base-foreground h-14 w-14 rounded-full fixed right-5 bottom-5 flex justify-center items-center"
-      >
-        <ArrowRightIcon className="text-skin-inverted w-8 h-8" />
+      <button {...props} className={twMerge(style, props.className)}>
+        {content}
       </button>
     </Link>
   )
