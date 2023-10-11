17802af048d86bd4dd1f0285f34e848ef16f65a9
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-10-11 12:30:38 -0300

    bug fix

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -9,7 +9,7 @@ export default function Separator({ ...props }: SeparatorProps) {
     <SeparatorPrimitive.Root
       {...props}
       className={twMerge(
-        `bg-zinc-600 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px mx-[15px]`,
+        `bg-zinc-600 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px`,
         props.className,
       )}
       decorative
