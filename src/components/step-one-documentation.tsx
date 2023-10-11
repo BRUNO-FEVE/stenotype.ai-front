bfa86d2d0a0a6708131d4aa0d371f0e62c7eb095
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-10-10 22:34:19 -0300

    right side bar added

--- src/components/step-one-documentation.tsx
+++ src/components/step-one-documentation.tsx
@@ -14,46 +14,57 @@ export default function StepOneDocumentation() {
   const { theme } = useContext(ThemeContext)
 
   return (
-    <div
-      className={`${
-        theme ? 'selection:bg-sky-300' : 'selection:bg-indigo-500'
-      } flex flex-col gap-10`}
-    >
-      <ContentBox>
-        <Text variant="title">Converting the Video</Text>
-        <Text variant="paragraph">
-          The first step in the Stenotype process is initiated after you upload
-          your video. Upon upload, we proceed to convert the video into an MP3
-          format, a crucial preparation step before sending it to Whisper, an
-          OpenAI API specialized in transcription services. It's important to
-          note that the file size of your video can significantly impact the
-          conversion time. Larger files may require more time for the conversion
-          process, so optimizing file sizes can expedite the transcription
-          process and lead to quicker results.
-        </Text>
-      </ContentBox>
-      <ContentBox className="gap-10">
-        <div className="flex flex-row items-center gap-4">
-          <TextCursorInputIcon className="w-8 h-8" />
-          <Text variant="subtitle_1">Key Words</Text>
+    <div className="relative">
+      <div className="w-1/5 fixed right-0 pl-2 text-sm hidden lg:flex lg:flex-col lg:gap-4">
+        <h2>On this page</h2>
+        <div className="flex flex-col gap-3">
+          <a href="#key-words">
+            <Text variant="link">Key Words</Text>
+          </a>
         </div>
-        <div className="flex justify-center">
-          <img
-            className="w-1/2"
-            src={theme ? ConvertingImgWhiteMode : ConvertingImgDarkMode}
-            alt="Converting Section Component"
-          ></img>
-        </div>
-        <Text variant="paragraph">
-          Moreover, paying attention to the inclusion of key words and ensuring
-          they are pronounced clearly in your video's audio content can
-          substantially improve the accuracy of the transcription. Whisper's
-          ability to recognize and transcribe key words is enhanced when they
-          are enunciated distinctly. This meticulous preparation ensures that
-          Whisper can accurately transcribe the audio content, setting the stage
-          for further content generation and analysis with Stenotype.
-        </Text>
-      </ContentBox>
+      </div>
+      <div
+        className={`${
+          theme ? 'selection:bg-sky-300' : 'selection:bg-indigo-500'
+        } flex flex-col gap-10`}
+      >
+        <ContentBox>
+          <Text variant="title">Converting the Video</Text>
+          <Text id="key-words" variant="paragraph">
+            The first step in the Stenotype process is initiated after you
+            upload your video. Upon upload, we proceed to convert the video into
+            an MP3 format, a crucial preparation step before sending it to
+            Whisper, an OpenAI API specialized in transcription services. It's
+            important to note that the file size of your video can significantly
+            impact the conversion time. Larger files may require more time for
+            the conversion process, so optimizing file sizes can expedite the
+            transcription process and lead to quicker results.
+          </Text>
+        </ContentBox>
+        <ContentBox className="gap-10">
+          <div className="flex flex-row items-center gap-4">
+            <TextCursorInputIcon className="w-8 h-8" />
+            <Text variant="subtitle_1">Key Words</Text>
+          </div>
+          <div className="flex justify-center">
+            <img
+              className="w-1/2"
+              src={theme ? ConvertingImgWhiteMode : ConvertingImgDarkMode}
+              alt="Converting Section Component"
+            ></img>
+          </div>
+          <Text variant="paragraph">
+            Moreover, paying attention to the inclusion of key words and
+            ensuring they are pronounced clearly in your video's audio content
+            can substantially improve the accuracy of the transcription.
+            Whisper's ability to recognize and transcribe key words is enhanced
+            when they are enunciated distinctly. This meticulous preparation
+            ensures that Whisper can accurately transcribe the audio content,
+            setting the stage for further content generation and analysis with
+            Stenotype.
+          </Text>
+        </ContentBox>
+      </div>
     </div>
   )
 }
